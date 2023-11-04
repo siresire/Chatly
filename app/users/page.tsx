6a24057bb516ee5a0b06180c64bd1b6ed871644d
Godfrey Bosire <godfreybosire@Godfreys-MacBook-Pro.local>
2023-11-04 11:19:50 +0300

    EmptyState

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,9 @@
-'use client';
-import { signOut } from "next-auth/react";
+import EmptyState from '../components/EmptyState';
 
 const Users = () => {
   return (
-    <div>
-      <button onClick={() => signOut()}>
-        Logout
-        </button>
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
     </div>
    );
 }
